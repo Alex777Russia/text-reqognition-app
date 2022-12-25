7c581ff11b2a351cb2ba37cb14ddd42281c204e9
Alex777Russia <shevalex777@mail.ru>
2022-12-25 11:29:13 +0300

    start project

---
+++ TextRecognition/TextRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by Алексей Шевченко on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
